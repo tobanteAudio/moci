3cfd3842a6c6056bf5bd0b2dfd24c49ed5b8c795
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-03-22 21:15:10 +0100

    Fix gl4 vertex array

--- moci/moci/render/vertex_array.cpp
+++ moci/moci/render/vertex_array.cpp
@@ -3,6 +3,7 @@
 #include "renderer.hpp"
 
 #include "moci/render/opengl/es2/vertex_array.hpp"
+#include "moci/render/opengl/gl4/vertex_array.hpp"
 
 namespace moci
 {
@@ -14,8 +15,9 @@ auto VertexArray::Create() -> std::shared_ptr<VertexArray>
         case RendererAPI::API::None:
             MOCI_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
             return nullptr;
-        // case RendererAPI::API::OpenGL: return std::make_shared<OpenGLVertexArray>();
+        case RendererAPI::API::OpenGL: return std::make_shared<OpenGLVertexArray>();
         case RendererAPI::API::OpenGLES: return std::make_shared<OpenGLESVertexArray>();
+        default: break;
     }
 
     MOCI_CORE_ASSERT(false, "Unknown RendererAPI!");
