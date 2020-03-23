f9c454fe336e467be2964c08b43ccb7121110efe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-23 22:31:18 +0000

    Deploy tobanteAudio/moci to github.com/tobanteAudio/moci.git:gh-pages

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['ignoreunused',['IgnoreUnused',['../namespacemoci.html#ae476b7800a4885ccb9a99952efba1628',1,'moci']]],
   ['imguilayer',['ImGuiLayer',['../classmoci_1_1ImGuiLayer.html#a646dc010366918b93e8d449e62d92ff2',1,'moci::ImGuiLayer::ImGuiLayer()'],['../classmoci_1_1ImGuiLayer.html#a68fb062aa7024f0b2fa1145931782bda',1,'moci::ImGuiLayer::ImGuiLayer(std::string const &amp;name)']]],
   ['init',['Init',['../classmoci_1_1Log.html#ac864bb61ffa4fbf71edf0dcf690d40b5',1,'moci::Log::Init()'],['../classmoci_1_1GraphicsContext.html#a5b40f6f2a4c68b1c147c4fca2144ae0c',1,'moci::GraphicsContext::Init()'],['../classmoci_1_1OpenGLESContext.html#a99114728d238f01316d59e1ac5281104',1,'moci::OpenGLESContext::Init()'],['../classmoci_1_1OpenGLESRendererAPI.html#a1f72a12200e7de7de4c89902b6e77b58',1,'moci::OpenGLESRendererAPI::Init()'],['../classmoci_1_1RenderCommand.html#a1c67a9b255b8b472628be246da68ee73',1,'moci::RenderCommand::Init()'],['../classmoci_1_1Renderer.html#a8e35af220df6d0f77217e6537ef35920',1,'moci::Renderer::Init()'],['../classmoci_1_1RendererAPI.html#ad9fa3b77a9bf0cfe7c4bd259c0d43cc2',1,'moci::RendererAPI::Init()']]],
   ['input',['Input',['../classmoci_1_1Input.html#aee364502d165f62af063b2010c6af76c',1,'moci::Input::Input()=default'],['../classmoci_1_1Input.html#adada4c9fbf643cafc2e9c7ed522bd0f9',1,'moci::Input::Input(const Input &amp;)=delete']]],
