36c254f70c5a974d4a755925f4441a8bee437893
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-05-27 21:17:52 +0200

    Update clang-tidy script

--- scripts/moci-clang-tidy.sh
+++ scripts/moci-clang-tidy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-python scripts/run-clang-tidy.py -header-filter="moci" -fix -p build_Debug moci
+cd build_Debug && ../scripts/run-clang-tidy.py ../moci/moci -p . -fix -header-filter="moci/moci/.*"
+cd build_Debug && ../scripts/run-clang-tidy.py ../sandbox -p . -fix -header-filter="moci/moci/.*"
+cd build_Debug && ../scripts/run-clang-tidy.py ../sandbox3D -p . -fix -header-filter="moci/moci/.*"
