3161267bf6daeb5c2fce30287fc9acf90d860ca5
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-08 10:25:18 +0200

    gcc: Slience assimp bugs

--- cmake/MociCompilerWarnings.cmake
+++ cmake/MociCompilerWarnings.cmake
@@ -4,61 +4,9 @@ add_library(Moci::CompilerWarnings ALIAS compiler_warnings)
 
 # Warnings
 if(MSVC)
-  target_compile_options(compiler_warnings INTERFACE /W4 "/permissive-")
+  target_compile_options(compiler_warnings INTERFACE /W4 "/permissive-" /wd4201)
 else()
-  target_compile_options(compiler_warnings
-                         INTERFACE -Wall
-                                    -Wno-int-to-void-pointer-cast
-                                  #  -Wextra # reasonable and standard
-                                  #  -Wshadow # warn the user if a variable
-                                  #           # declaration shadows one from a
-                                  #           # parent context
-                                  #  -Wnon-virtual-dtor # warn the user if a class
-                                  #                     # with virtual functions
-                                  #                     # has a non-virtual
-                                  #                     # destructor. This helps
-                                  #                     # catch hard to track down
-                                  #                     # memory errors
-                                  #  -Wold-style-cast # warn for c-style casts
-                                  #  -Wcast-align # warn for potential performance
-                                  #               # problem casts
-                                  #  -Wunused # warn on anything being unused
-                                  #  -Woverloaded-virtual # warn if you overload
-                                  #                       # (not override) a
-                                  #                       # virtual function
-                                  #  -Wpedantic # warn if non-standard C++ is used
-                                  #  -Wconversion # warn on type conversions that
-                                  #               # may lose data
-                                  #  -Wsign-conversion # warn on sign conversions
-                                  #  -Wnull-dereference # warn if a null
-                                  #                     # dereference is detected
-                                  #  -Wdouble-promotion # warn if float is
-                                  #                     # implicit promoted to
-                                  #                     # double
-                                  #  -Wformat=2 # warn on security issues around
-                                  #             # functions that format output (ie
-                                  #             # printf)
-                         )
-
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    target_compile_options(compiler_warnings
-                           INTERFACE -Wmisleading-indentation # warn if
-                                                              # identation
-                                    #                           # implies blocks
-                                    #                           # where blocks do
-                                    #                           # not exist
-                                    #  -Wduplicated-cond # warn if if / else chain
-                                    #                    # has duplicated
-                                    #                    # conditions
-                                    #  -Wduplicated-branches # warn if if / else
-                                    #                        # branches have
-                                    #                        # duplicated code
-                                    #  -Wlogical-op # warn about logical
-                                    #               # operations being used where
-                                    #               # bitwise were probably wanted
-                                    #  -Wuseless-cast # warn if you perform a cast
-                                    #                 # to the same type
-                           )
-  else()
-  endif()
+  target_compile_options(compiler_warnings INTERFACE -Wall -Wextra -Wshadow)
+  # Bugs in assimp
+  add_compile_options(-Wno-error=tautological-compare -Wno-error=class-memaccess)
 endif()
