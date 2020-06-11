b1193c99a0b6aaba25ce5bc897411beec15f0fa7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-11 01:59:02 +0000

    Deploy tobanteAudio/moci to github.com/tobanteAudio/moci.git:gh-pages

--- search/all_15.js
+++ search/all_15.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['white',['White',['../classmoci_1_1Colors.html#a5b6b4d6780455cb2cc1af38dd9ab4f19',1,'moci::Colors']]],
-  ['width',['width',['../structmoci_1_1Typeface_1_1Character.html#a94785c762065ca07c33bd067797c93d0',1,'moci::Typeface::Character::width()'],['../structmoci_1_1WindowSpecs.html#a38dce1c480cab1234fd77c70bb1ae39d',1,'moci::WindowSpecs::Width()']]],
+  ['width',['Width',['../structmoci_1_1WindowSpecs.html#a38dce1c480cab1234fd77c70bb1ae39d',1,'moci::WindowSpecs::Width()'],['../structmoci_1_1Typeface_1_1Character.html#a94785c762065ca07c33bd067797c93d0',1,'moci::Typeface::Character::width()']]],
   ['window',['Window',['../classmoci_1_1Window.html',1,'moci']]],
   ['windowclose',['WindowClose',['../namespacemoci.html#a1ffc575f64d8c1ee66b01f44bbb10036ac984201d37197ab297a14d19efee4b07',1,'moci']]],
   ['windowcloseevent',['WindowCloseEvent',['../classmoci_1_1WindowCloseEvent.html',1,'moci::WindowCloseEvent'],['../classmoci_1_1WindowCloseEvent.html#abb82e7e4407d698d480b9c52f3dadc9b',1,'moci::WindowCloseEvent::WindowCloseEvent()']]],
