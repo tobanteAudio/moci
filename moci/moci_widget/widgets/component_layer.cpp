3c25b09ed10c87e0e97cd5c1be0d2a48bb4d4430
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-13 04:41:58 +0200

    widget: Refactor event handler in component layer

--- moci/moci_widget/widgets/component_layer.cpp
+++ moci/moci_widget/widgets/component_layer.cpp
@@ -1,1 +1,76 @@
 #include "moci_widget/widgets/component_layer.hpp"
+
+namespace moci
+{
+void ComponentLayer::OnAttach()
+{
+    renderQueue_ = MakeScope<RenderQueue>();
+    rootComponent_->SetStyle(&defaultStyle_);
+    rootComponent_->SetSize(static_cast<int>(width_), static_cast<int>(height_));
+}
+
+void ComponentLayer::OnDetach() { }
+
+void ComponentLayer::OnUpdate(Timestep ts)
+{
+    IgnoreUnused(ts);
+    // MOCI_CORE_ASSERT(rootComponent_ != nullptr);
+    RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
+    RenderCommand::Clear();
+
+    renderQueue_->StartFrame(width_, height_);
+    rootComponent_->SetPosition(0, 0);
+    rootComponent_->Draw(*renderQueue_);
+    renderQueue_->EndFrame();
+}
+
+void ComponentLayer::OnImGuiRender()
+{
+    ImGui::Begin("Settings");
+    ImGui::Text("Draws: %d", renderQueue_->GetStats().drawCount);
+    ImGui::Text("Textures: %d", renderQueue_->GetStats().textureCount);
+    ImGui::Text("Vertics: %d", renderQueue_->GetStats().vertexCount);
+    ImGui::Text("Quads: %d", renderQueue_->GetStats().quadCount);
+    ImGui::Text("Circles: %d", renderQueue_->GetStats().circleCount);
+    ImGui::End();
+}
+
+void ComponentLayer::OnEvent(Event& e)
+{
+    auto dispatcher = EventDispatcher {e};
+    dispatcher.Dispatch<WindowResizeEvent>([self = this](auto& e) { return self->onWindowResized(e); });
+    dispatcher.Dispatch<MouseMovedEvent>([self = this](auto& e) { return self->onMouseMoved(e); });
+    dispatcher.Dispatch<MouseScrolledEvent>([self = this](auto& e) { return self->onMouseScrolled(e); });
+    dispatcher.Dispatch<MouseButtonPressedEvent>([self = this](auto& e) { return self->onMousePressed(e); });
+}
+
+bool ComponentLayer::onWindowResized(WindowResizeEvent& e)
+{
+    width_  = static_cast<float>(e.GetWidth());
+    height_ = static_cast<float>(e.GetHeight());
+    rootComponent_->SetSize(static_cast<int>(width_), static_cast<int>(height_));
+    return false;
+}
+
+bool ComponentLayer::onMouseMoved(MouseMovedEvent& e)
+{
+    IgnoreUnused(e);
+    // MOCI_CORE_INFO("Mouse: {}, {}", e.GetX(), e.GetY());
+    return true;
+}
+
+bool ComponentLayer::onMouseScrolled(MouseScrolledEvent& e)
+{
+    MOCI_CORE_INFO("Mouse: {}, {}", e.GetXOffset(), e.GetYOffset());
+    return true;
+}
+
+bool ComponentLayer::onMousePressed(MouseButtonPressedEvent& e)
+{
+    IgnoreUnused(e);
+    auto const x = static_cast<int>(Input::GetMouseX());
+    auto const y = static_cast<int>(Input::GetMouseY());
+    return handleEvent([&](auto* comp) { return comp->MouseClicked(MouseCallback::Click {x, y}); });
+}
+
+}  // namespace moci
--- moci/moci_widget/widgets/component_layer.hpp
+++ moci/moci_widget/widgets/component_layer.hpp
@@ -19,73 +19,27 @@ public:
     ComponentLayer(Scope<Component>&& root) : Layer("component-layer"), rootComponent_(std::move(root)) { }
     ~ComponentLayer() override = default;
 
-    void OnAttach() override
+    void OnAttach() override;
+    void OnDetach() override;
+    void OnUpdate(Timestep ts) override;
+    void OnImGuiRender() override;
+    void OnEvent(Event& e) override;
+
+private:
+    bool onWindowResized(WindowResizeEvent& e);
+    bool onMouseMoved(MouseMovedEvent& e);
+    bool onMouseScrolled(MouseScrolledEvent& e);
+    bool onMousePressed(MouseButtonPressedEvent& e);
+
+    template<typename Handler>
+    bool handleEvent(Handler handler)
     {
-        renderQueue_ = MakeScope<RenderQueue>();
-        rootComponent_->SetStyle(&defaultStyle_);
-        rootComponent_->SetSize(static_cast<int>(width_), static_cast<int>(height_));
-    }
-
-    void OnDetach() override { }
-
-    void OnUpdate(Timestep ts) override
-    {
-        IgnoreUnused(ts);
-        // MOCI_CORE_ASSERT(rootComponent_ != nullptr);
-        RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
-        RenderCommand::Clear();
-
-        renderQueue_->StartFrame(width_, height_);
-        rootComponent_->SetPosition(0, 0);
-        rootComponent_->Draw(*renderQueue_);
-        renderQueue_->EndFrame();
-    }
-
-    void OnImGuiRender() override
-    {
-        ImGui::Begin("Settings");
-        ImGui::Text("Draws: %d", renderQueue_->GetStats().drawCount);
-        ImGui::Text("Textures: %d", renderQueue_->GetStats().textureCount);
-        ImGui::Text("Vertics: %d", renderQueue_->GetStats().vertexCount);
-        ImGui::Text("Quads: %d", renderQueue_->GetStats().quadCount);
-        ImGui::Text("Circles: %d", renderQueue_->GetStats().circleCount);
-        ImGui::End();
-    }
-
-    void OnEvent(Event& e) override
-    {
-        auto dispatcher = EventDispatcher {e};
-        dispatcher.Dispatch<WindowResizeEvent>(MOCI_EVENT_METHOD(ComponentLayer::OnWindowResized));
-        dispatcher.Dispatch<MouseMovedEvent>(MOCI_EVENT_METHOD(ComponentLayer::OnMouseMoved));
-        dispatcher.Dispatch<MouseButtonPressedEvent>(MOCI_EVENT_METHOD(ComponentLayer::OnMousePressed));
-    }
-
-    bool OnWindowResized(WindowResizeEvent& e)
-    {
-        width_  = static_cast<float>(e.GetWidth());
-        height_ = static_cast<float>(e.GetHeight());
-        rootComponent_->SetSize(static_cast<int>(width_), static_cast<int>(height_));
-        return false;
-    }
-
-    bool OnMouseMoved(MouseMovedEvent& e)
-    {
-        IgnoreUnused(e);
-        // MOCI_CORE_INFO("Mouse: {}, {}", e.GetX(), e.GetY());
-        return true;
-    }
-
-    bool OnMousePressed(MouseButtonPressedEvent& e)
-    {
-
-        auto const code = e.GetMouseButton();
-        IgnoreUnused(code);
         auto const x = static_cast<int>(Input::GetMouseX());
         auto const y = static_cast<int>(Input::GetMouseY());
         auto* comp   = rootComponent_->FindComponentAt({x, y});
-        MOCI_CORE_ASSERT(comp, "Should never be null");
 
-        while (!comp->MouseClicked(MouseCallback::Click {x, y}))
+        MOCI_CORE_ASSERT(comp, "Should never be null");
+        while (!handler(comp))
         {
             comp = comp->GetParent();
             if (comp == nullptr)
@@ -97,7 +51,6 @@ public:
         return true;
     }
 
-private:
     float width_  = 1280.0f;
     float height_ = 1024.0f;
 
