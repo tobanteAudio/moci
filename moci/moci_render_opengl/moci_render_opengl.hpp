dacacd9507b6dd0188e6f3cf9e0d2577fd67666d
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-14 03:15:40 +0200

    opengl: Fix module includes

--- moci/moci_render_opengl/moci_render_opengl.hpp
+++ moci/moci_render_opengl/moci_render_opengl.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "moci_render_opengl/buffer.hpp"
+#include "moci_render_opengl/gl4.hpp"
+#include "moci_render_opengl/graphics_context.hpp"
+#include "moci_render_opengl/renderer_api.hpp"
+#include "moci_render_opengl/shader.hpp"
+#include "moci_render_opengl/texture.hpp"
+#include "moci_render_opengl/vertex_array.hpp"
