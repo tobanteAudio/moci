9b78fbf8a4a99208469be5d5afb100254e22f0cf
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-14 02:04:12 +0200

    events: Add macro aliases to mouse codes

--- moci/moci_events/mouse_button_codes.hpp
+++ moci/moci_events/mouse_button_codes.hpp
@@ -1,23 +1,46 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 
-// From glfw3.h
 namespace moci
 {
-enum class MouseCode : std::uint8_t
+// From glfw3.h
+enum class MouseCode : uint16_t
 {
-    Button1 = 0,
-    Button2 = 1,
-    Button3 = 2,
-    Button4 = 3,
-    Button5 = 4,
-    Button6 = 5,
-    Button7 = 6,
-    Button8 = 7,
+    Button0 = 0,
+    Button1 = 1,
+    Button2 = 2,
+    Button3 = 3,
+    Button4 = 4,
+    Button5 = 5,
+    Button6 = 6,
+    Button7 = 7,
 
-    ButtonLeft   = 0,
-    ButtonRight  = 1,
-    ButtonMiddle = 2,
+    ButtonLast   = Button7,
+    ButtonLeft   = Button0,
+    ButtonRight  = Button1,
+    ButtonMiddle = Button2
 };
+
+using Mouse = MouseCode;
+
+inline std::ostream& operator<<(std::ostream& os, MouseCode mouseCode)
+{
+    os << static_cast<int32_t>(mouseCode);
+    return os;
 }
+}  // namespace moci
+
+#define MOCI_MOUSE_BUTTON_0 ::moci::Mouse::Button0
+#define MOCI_MOUSE_BUTTON_1 ::moci::Mouse::Button1
+#define MOCI_MOUSE_BUTTON_2 ::moci::Mouse::Button2
+#define MOCI_MOUSE_BUTTON_3 ::moci::Mouse::Button3
+#define MOCI_MOUSE_BUTTON_4 ::moci::Mouse::Button4
+#define MOCI_MOUSE_BUTTON_5 ::moci::Mouse::Button5
+#define MOCI_MOUSE_BUTTON_6 ::moci::Mouse::Button6
+#define MOCI_MOUSE_BUTTON_7 ::moci::Mouse::Button7
+#define MOCI_MOUSE_BUTTON_LAST ::moci::Mouse::ButtonLast
+#define MOCI_MOUSE_BUTTON_LEFT ::moci::Mouse::ButtonLeft
+#define MOCI_MOUSE_BUTTON_RIGHT ::moci::Mouse::ButtonRight
+#define MOCI_MOUSE_BUTTON_MIDDLE ::moci::Mouse::ButtonMiddle
