8281d06b921b72cebcac66f7ca953b55decba1d4
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-15 01:19:34 +0200

    sandoxOpenGL: Add es2 shader version

--- assets/shader/basic_es2.glsl
+++ assets/shader/basic_es2.glsl
@@ -1,19 +1,22 @@
 #shader vertex
 
 attribute vec3 position;
+attribute vec4 color;
 
-uniform mat4 u_MVP;
+varying vec4 v_Color;
 
-void main() { gl_Position = u_MVP * vec4(position, 1.0); }
+void main()
+{
+    v_Color     = color;
+    gl_Position = vec4(position, 1.0);
+}
 
 #shader fragment
 
 #ifdef GL_ES
 precision mediump float;
-#elif
-layout(location = 0) out vec4 gl_FragColor;
 #endif
 
-uniform vec4 u_Color;
+varying vec4 v_Color;
 
-void main() { gl_FragColor = u_Color; }
+void main() { gl_FragColor = v_Color; }
