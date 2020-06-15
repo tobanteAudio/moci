8c72022d1bc5970e1218bdb6618a514e69a9e64d
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-16 01:30:01 +0200

    opengles: Fix framebuffer texture attachment

--- moci/moci_render_opengles/es2/frame_buffer.cpp
+++ moci/moci_render_opengles/es2/frame_buffer.cpp
@@ -15,51 +15,15 @@ void OpenGLESFramebuffer::Invalidate()
     GLCall(glGenFramebuffers(1, &renderID_));
     GLCall(glBindFramebuffer(GL_FRAMEBUFFER, renderID_));
 
-    // GLCall(glGenTextures(1, &colorAttachment_));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, colorAttachment_));
-    // GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, specs_.width, specs_.height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-    // nullptr)); GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-    // GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment_, 0));
+    GLCall(glGenTextures(1, &colorAttachment_));
+    GLCall(glBindTexture(GL_TEXTURE_2D, colorAttachment_));
+    GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, specs_.width, specs_.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+    GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment_, 0));
 
-    // GLCall(glGenTextures(1, &depthAttachment_));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, depthAttachment_));
-    // GLCall(glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT, specs_.width, specs_.height));
-    // GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthAttachment_, 0));
-
-    // MOCI_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is
-    // incomplete!");
+    MOCI_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // GLCall(glGenFramebuffers(1, &renderID_));
-    // GLCall(glBindFramebuffer(GL_FRAMEBUFFER, renderID_));
-
-    // GLCall(glGenTextures(1, &colorAttachment_));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, colorAttachment_));
-
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-    // // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
-    // // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
-
-    // GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, specs_.width, specs_.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, 0));
-
-    // // Build the texture that will serve as the depth attachment for the renderID_.
-    // GLCall(glGenTextures(1, &depthAttachment_));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, depthAttachment_));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-    // GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-    // GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, specs_.width, specs_.height, 0, GL_DEPTH_COMPONENT,
-    //                     GL_UNSIGNED_BYTE, NULL));
-    // GLCall(glBindTexture(GL_TEXTURE_2D, 0));
-
-    // GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment_, 0));
-    // GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthAttachment_, 0));
-    // MOCI_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "framebuffer is
-    // incomplete"); GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
 
 void OpenGLESFramebuffer::Bind() { glBindFramebuffer(GL_FRAMEBUFFER, renderID_); }
