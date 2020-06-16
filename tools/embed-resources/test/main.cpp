409352fb20e9ab5c04320f942350765da8c1ea34
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-16 18:42:38 +0200

    tools: Add example to embed script

---
+++ tools/embed-resources/test/main.cpp
@@ -0,0 +1,17 @@
+#include <sstream>
+
+#include "basic_gl4.hpp"
+
+int main()
+{
+    auto const shaderSource = std::string {moci::resources::basic_gl4, moci::resources::basic_gl4_len};
+    auto line               = std::string {};
+    auto input              = std::istringstream {shaderSource};
+
+    while (getline(input, line))
+    {
+        std::printf("%s\n", line.c_str());
+    }
+
+    return 0;
+}
