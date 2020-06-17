9b74a4191f7c873cd0f26c5b687c79375400060c
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-06-17 02:31:02 +0200

    opengl: Fix warnings in shader

--- moci/moci_render_opengl/gl4/shader.cpp
+++ moci/moci_render_opengl/gl4/shader.cpp
@@ -13,7 +13,9 @@
 namespace moci
 {
 
-static GLenum OpenGLShaderTypeFromShaderType(ShaderType type)
+namespace
+{
+GLenum OpenGLShaderTypeFromShaderType(ShaderType type)
 {
     if (type == ShaderType::Vertex)
     {
@@ -28,6 +30,7 @@ static GLenum OpenGLShaderTypeFromShaderType(ShaderType type)
     return 0;
 }
 
+}  // namespace
 OpenGLShader::OpenGLShader(std::string const& filepath)
 {
     MOCI_PROFILE_FUNCTION();
@@ -67,29 +70,15 @@ std::string OpenGLShader::ReadFile(std::string const& filepath)
     MOCI_PROFILE_FUNCTION();
 
     std::string result;
-    std::ifstream in(filepath, std::ios::in | std::ios::binary);
-    if (in)
+    std::ifstream file(filepath, std::ios::in | std::ios::binary);
+    if (file)
     {
-        in.seekg(0, std::ios::end);
-        size_t size = in.tellg();
-        if (size != -1)
-        {
-            result.resize(size);
-            in.seekg(0, std::ios::beg);
-            in.read(&result[0], size);
-            in.close();
-        }
-        else
-        {
-            MOCI_CORE_ERROR("Could not read from file '{0}'", filepath);
-        }
-    }
-    else
-    {
-        MOCI_CORE_ERROR("Could not open file '{0}'", filepath);
+        auto ss = std::ostringstream {};
+        ss << file.rdbuf();
+        return ss.str();
     }
 
-    return result;
+    return {};
 }
 
 std::unordered_map<GLenum, std::string> OpenGLShader::PreProcess(std::string const& source)
