853b8f67bc1f9e5cf3382fa43c79195f8653209a
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-02 08:49:24 +0200

    Use conan for spdlog

--- cmake/MociCompilerWarnings.cmake
+++ cmake/MociCompilerWarnings.cmake
@@ -7,9 +7,4 @@ if(MSVC)
   target_compile_options(compiler_warnings INTERFACE /W4 "/permissive-" /wd4201)
 else()
   target_compile_options(compiler_warnings INTERFACE -Wall -Wextra -Wpedantic -Wno-shadow)
-
-  # Bugs in assimp
-  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    add_compile_options(-Wno-error=tautological-compare -Wno-error=class-memaccess)
-  endif()
 endif()
