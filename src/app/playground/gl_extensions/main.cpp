220ffeb8a90234c61db84ab09f20a30da2c279a3
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-10-30 23:32:17 +0100

    Add gl_extsions tool

---
+++ src/app/playground/gl_extensions/main.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+auto main() -> int
+{
+    if (auto success = glfwInit(); success == GLFW_FALSE)
+    {
+        std::fprintf(stderr, "Failed to initialize glfw\n");
+        return EXIT_FAILURE;
+    }
+
+    auto window = glfwCreateWindow(1280, 720, "Window", nullptr, nullptr);
+    glfwHideWindow(window);
+    glfwMakeContextCurrent(window);
+
+    if (auto err = glewInit() != GLEW_OK; err)
+    {
+        auto const* msg = glewGetErrorString(err);
+        std::fprintf(stderr, "Failed to initialize glew: %s\n", reinterpret_cast<char const*>(msg));
+        return EXIT_FAILURE;
+    }
+
+    GLint n = 0;
+    glGetIntegerv(GL_NUM_EXTENSIONS, &n);
+    std::printf("num: %d\n", n);
+
+    for (GLint i = 0; i < n; i++)
+    {
+        const char* extension = (const char*)glGetStringi(GL_EXTENSIONS, i);
+        printf("Ext %d: %s\n", i, extension);
+    }
+
+    return EXIT_SUCCESS;
+}
